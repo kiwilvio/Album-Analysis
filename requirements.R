c0a5fd7855e42a01dc6108b558e4641f07481c6a
quilvioo <94944913+quilvioo@users.noreply.github.com>
2022-03-04 16:10:32 -0500

    file organization and requirement file

---
+++ requirements.R
@@ -0,0 +1,19 @@
+install.packages("shiny") # Package for the R Shiny interactions
+install.packages("shinythemes") # Themes for Shiny App
+install.packages("tidyverse") # Data Manipulation
+install.packages("geniusr") # Genius API Wrapper
+install.packages("spotifyr") # Spotify API Wrapper
+install.packages("jsonlite") # JSON parser
+install.packages("tidytext") # Text Sentiments
+install.packages("tm") # Text Mining Framework
+install.packages("textdata") # Sentiment Lexicons
+install.packages("wordcloud") # Word Clouds
+install.packages("reshape2") # Postive-Negative Word Cloud
+install.packages("proxy") # Dendrogram
+install.packages("igraph") # Network Graphs
+install.packages("ggraph") # Network Graphs
+install.packages("ggthemes") # Themes for plots
+install.packages("ggrepel") # Text Labels for ggplot
+install.packages("ggridges") # Density Ridges ggplot
+install.packages("wesanderson") # Wes Anderson Color Palatte
+install.packages("radarchart") # Radar Graph Package
